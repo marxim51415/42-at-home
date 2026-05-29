b4f5a62f65e0f6b49d7503c9eacc7123bf1844ff
max perso <maxime81261@gmail.com>
2026-05-29 02:29:33 +0200

    fixed itoa much quicker than strtrim

--- ft_itoa.c
+++ ft_itoa.c
@@ -14,20 +14,15 @@
 #include <string.h>
 #include <stdlib.h>
 
-size_t	order_magnitude(int nb)			//next try to see if i can merge these conditions on the same line?
+size_t	order_magnitude(int nb)
 {
 	size_t	i;
 
-	i = 1;
-	if (nb >= 0)
+	i = 0;
+	while (nb != 0)
 	{
-		while (nb / 10 >= 1)
-			i++;
-	}
-	else
-	{
-		while (nb / 10 <= -1)
-			i++;
+		nb /= 10;
+		i++;
 	}
 	return (i);
 }
@@ -35,11 +30,8 @@ size_t	order_magnitude(int nb)			//next try to see if i can merge these conditio
 char	*work_horse(char *result, size_t nb, size_t i)
 {
 	if (nb >= 10)
-		result = work_horse(result, nb / 10, --i);
-//	printf("result[i] is %d\n", result[i]);
+		result = work_horse(result, nb / 10, i - 1);
 	result[i] = nb % 10 + 48;
-//	printf("then result[i] becomes %d\n", result[i]);
-//	printf("at loop end result is %s\n", result);
 	return (result);
 }
 
@@ -48,9 +40,8 @@ char	*ft_itoa(int n)
 	char	*result;
 	size_t	order;
 
-	order = 3;
-//	order = order_magnitude(n);
-	if (n < 0)
+	order = order_magnitude(n);
+	if (n <= 0)
 		order++;
 	result = malloc(order + 1);
 	if (!result)
@@ -61,18 +52,13 @@ char	*ft_itoa(int n)
 		n *= -1;
 	}
 	result = work_horse(result, n, order - 1);
-
-//	int j = 0;
-//	while (j < 4)
-//		printf("%c\n", result[j++]);
 	result[order] = 0;
-	printf("\n\nat program end result is : %s\n", result);
 	return (result);
 }
 
 int main()
 {
-	char *toa = ft_itoa(-568);
+	char *toa = ft_itoa(3000);
 	printf("main() result is : %s\n", toa);
 	free(toa);
 }
